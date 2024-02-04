7d951bcb69f1cc7be9ee25f17f72d50c6817e911
Sovandas_x <72575459+sovandas089@users.noreply.github.com>
2024-02-04 16:24:38 +0000

    javaScript with Chai

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 890212
+let accountEmail = "sovan@gamil.com"
+var accountPassword = "12345"
+accountCity = "Bilaspur"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
