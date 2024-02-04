1e937b49fa8a42e8fbc237f65fcb0247aed8548d
Sovandas_x <72575459+sovandas089@users.noreply.github.com>
2024-02-04 16:45:52 +0000

    commited

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,4 +4,12 @@ var accountPassword = "12345"
 accountCity = "Bilaspur"
 let accountState;
 
+// const = constant will never change if it declared once
+/*
+Prefer not to use var variable
+Because of issue in block scope and functional scope
+*/
+
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// console.table is used for getting output in a table 
